bf34e1dcbf3efd1bab8ca6cafd7f1eb63b2e9904
kiran <kiransaidu179@gmail.com>
2024-02-27 17:15:55 +0530

    First Comment

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int num1 = 1; num1 <= 10; num1++)
+{
+    Console.WriteLine(num1);
+    Thread.Sleep(1000);
+
+}
+Console.WriteLine("10 t0 100");
+for(int num1 = 10;num1<=100;num1++)
+{
+    Console.WriteLine(num1);
+    Thread.Sleep(100);
+
+}
+
